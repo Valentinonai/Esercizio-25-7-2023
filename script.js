9899c7e50a231cf257f3f3558819eb0f20da0cf4
Valentinonai <137069763+Valentinonai@users.noreply.github.com>
2023-07-25 18:45:39 +0200

    v1.2 Aggiunto Js per nascondere e far apparire bottoni in base allo scroll

---
+++ script.js
@@ -0,0 +1,14 @@
+const showButton = () => {
+  const backTop = document.getElementById("backTop");
+  if (window.scrollY > 500) {
+    backTop.style.display = "inline-block";
+    document.documentElement.style.setProperty("--social--height", `${115}px`);
+    document.documentElement.style.setProperty("--calendar--height", `${60}px`);
+  } else {
+    backTop.style.display = "none";
+    document.documentElement.style.setProperty("--social--height", `${60}px`);
+    document.documentElement.style.setProperty("--calendar--height", `${10}px`);
+  }
+};
+
+window.addEventListener("scroll", showButton);
