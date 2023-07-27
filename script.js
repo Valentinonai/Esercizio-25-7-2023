08be8a4335cf590a2c79dbe0a861a41505e10e09
Valentinonai <137069763+Valentinonai@users.noreply.github.com>
2023-07-27 16:40:13 +0200

    v2.5 ottimizzato codice Js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const showButton = () => {
   const backTop = document.getElementById("backTop");
-  if (window.scrollY > 500) {
+  // if (window.scrollY > 500) {
+  //   backTop.style.display = "inline-block";
+  //   document.documentElement.style.setProperty("--social--height", `${115}px`);
+  //   document.documentElement.style.setProperty("--calendar--height", `${60}px`);
+  // } else {
+  //   backTop.style.display = "none";
+  //   document.documentElement.style.setProperty("--social--height", `${60}px`);
+  //   document.documentElement.style.setProperty("--calendar--height", `${10}px`);
+  // }
+  if (document.body.getBoundingClientRect().y < -500) {
     backTop.style.display = "inline-block";
     document.documentElement.style.setProperty("--social--height", `${115}px`);
     document.documentElement.style.setProperty("--calendar--height", `${60}px`);
